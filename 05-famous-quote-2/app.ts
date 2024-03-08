129414bd7acba9ebc71160d06c61b8b4f4bb6248
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-08 14:27:28 +0000

    done

--- 05-famous-quote-2/app.ts
+++ 05-famous-quote-2/app.ts
@@ -2,11 +2,11 @@
 // Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
 // person name
-let famous_person = 'Muhammad Ali Jinnah';
+let famous_person:string = 'Muhammad Ali Jinnah';
 // person quote
-let famous_quote = 'Think 100 times before you take a decision, But once that decision is taken, stand by it as one man.';
+let famous_quote:string = 'Think 100 times before you take a decision, But once that decision is taken, stand by it as one man.';
 // message with person and and his quote
-let message = `${famous_person} once said, "${famous_quote}"`;
+let message:string = `${famous_person} once said, "${famous_quote}"`;
 // print message
 console.log(message);
 
