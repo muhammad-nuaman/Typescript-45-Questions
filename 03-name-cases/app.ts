b97fcdc2c556cd7ff4e2ba27c47d190c1627398f
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-09 19:23:47 +0000

    done

--- 03-name-cases/app.ts
+++ 03-name-cases/app.ts
@@ -1,17 +1,13 @@
 // Task No: 3
 /*Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase. */
 
-let second_person:string = 'sir zia khan';
+let second_person:string = 'salman';
 // lower case
 console.log(second_person.toLowerCase());
 // upper case
 console.log(second_person.toUpperCase());
 
-let $second_person: string[] = second_person.split(" ");
-$second_person.forEach((e, i, arr)=>{
-    arr[i] = e[0].toUpperCase()+e.slice(1).toLowerCase();
-});
-
-// title case
-console.log(...$second_person);
+// title case 
+second_person = second_person[0].toUpperCase() + second_person.slice(1).toLowerCase();
+console.log(second_person);
 
