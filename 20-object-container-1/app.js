1464c4ebd1642cec5d2e76a7f32de6e7e9e19030
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-17 15:14:36 +0000

    done

---
+++ 20-object-container-1/app.js
@@ -0,0 +1,17 @@
+/*
+<<<< Task No: 20 >>>>
+Think of something you could store in a array.
+For example, you could make a list of mountains,
+rivers, countries, cities, languages, or anything else you’d like.
+Write a program that creates a list containing these items.
+*/
+// Function: create list
+const lister = (...list) => list;
+// mountains list
+const mountains = lister('k2', 'Mount Everest');
+// cities list
+const cities = lister('Karachi', 'Lahore');
+// Print Program
+console.table(mountains);
+console.table(cities);
+export {};
