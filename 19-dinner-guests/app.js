be181983a818d8fb4774fabd72af8ce7d079d652
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-17 14:45:30 +0000

    done

---
+++ 19-dinner-guests/app.js
@@ -0,0 +1,14 @@
+/*
+<<<< Task No: 19 >>>>
+Dinner Guests: Working with one of the programs from Exercises 14
+through 18, print a message indicating the number of people
+you are inviting to dinner.
+ */
+let guests = [
+    'Bill Gates',
+    'Mark Zuckerberg',
+    'Elon Musk',
+    'Steve Jobs'
+];
+console.log(`I am inviting ${guests.length} people to dinner.`);
+export {};
---
+++ 19-dinner-guests/app.ts
@@ -0,0 +1,17 @@
+/*
+<<<< Task No: 19 >>>>
+Dinner Guests: Working with one of the programs from Exercises 14
+through 18, print a message indicating the number of people 
+you are inviting to dinner.
+ */
+
+// Dinner Guests list
+let guests: string[] = [
+    'Bill Gates', 
+    'Mark Zuckerberg', 
+    'Elon Musk', 
+    'Steve Jobs'
+];
+
+// number of guests 
+console.log(`I was inviting ${guests.length} people to dinner.`);
