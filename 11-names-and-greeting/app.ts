9c59a4d3dde758670b538a6cba6648a81c5c3f2c
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-17 01:05:13 +0000

    done

---
+++ 11-names-and-greeting/app.ts
@@ -0,0 +1,29 @@
+/* 
+<<<< Task No: 11 >>>> 
+Names: Store the names of a few of your friends in a 
+array called names. Print each person’s name by 
+accessing each element in the list, one at a time.
+*/
+
+// My Friends Name
+const friends: string[] = ['Kashif', 'Husnain', 'Zeeshan', 'Bilal'];
+
+// Print Frinds Name
+console.group('My Friends Name: ');
+friends.forEach((name, index) => console.log(`${(++index)}: ${name}`));
+console.groupEnd();
+
+
+/*
+<<<< Task No: 12 >>>> 
+Greetings: Start with the array you used in Exercise 11, 
+but instead of just printing each person’s name, 
+print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+*/
+
+// Greeting to Friends
+console.group('\nGreetings:');
+for (let friend of friends) {
+    console.log(`"Hi ${friend}, hope you're having an amazing day!"`)
+}
+console.groupEnd();
---
+++ 18-seeing-world/app.js
@@ -0,0 +1,34 @@
+/*
+<<<< Task No: 18 >>>>
+Seeing the World:
+Think of at least five places in the world you’d like to visit.
+*/
+let places = ['usa', 'canada', 'turkey', 'japan', 'germany'];
+// 1. original
+console.dir('Original');
+console.table(places);
+// 2. alphabetic
+console.dir('Alphabetical');
+console.table([...places].sort());
+// 3. original
+console.dir('Original');
+console.table(places);
+// 4. reverse alphabetical
+console.dir('Reverse Alphabetical');
+console.table([...places].sort().reverse());
+// 5. original
+console.dir('Original');
+console.table(places);
+// 6. original reverse 
+console.dir('Original Reverse');
+console.table(places.reverse());
+// 7. original reverse again
+console.dir('Original Reverse Again');
+console.table(places.reverse());
+// 8. original alphabetical
+console.dir('Original Alphabetical');
+console.table(places.sort());
+// 9. original reverse alphabetical
+console.dir('Original Reverse Alphabetical');
+console.table(places.sort().reverse());
+export {};
