9fd359a6b968179737545c45790f1a7a93327089
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-17 14:46:30 +0000

    done

--- 19-dinner-guests/app.js
+++ 19-dinner-guests/app.js
@@ -4,11 +4,13 @@ Dinner Guests: Working with one of the programs from Exercises 14
 through 18, print a message indicating the number of people
 you are inviting to dinner.
  */
+// Dinner Guests list
 let guests = [
     'Bill Gates',
     'Mark Zuckerberg',
     'Elon Musk',
     'Steve Jobs'
 ];
-console.log(`I am inviting ${guests.length} people to dinner.`);
+// number of guests 
+console.log(`I was inviting ${guests.length} people to dinner.`);
 export {};
