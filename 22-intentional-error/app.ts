6fe73c05c69bdf7cfb643307cae6f2b168ef7b34
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-17 16:09:15 +0000

    done

---
+++ 22-intentional-error/app.ts
@@ -0,0 +1,19 @@
+/*
+<<<< Task No: 22 >>>>
+Intentional Error: 
+If you haven’t received an array index error in one of 
+your programs yet, try to make one happen. 
+Change an index in one of your programs to produce 
+an index error. Make sure you correct the error 
+before closing the program.
+*/
+
+// A Grocery List
+let grocery: string[] = ["Rice", "Butter", "Chicken"];
+
+// Intentational Error
+// console.log(grocery['Rice']); 
+
+// Indexing
+console.log(grocery[0]);
+console.log(grocery['0']); 
