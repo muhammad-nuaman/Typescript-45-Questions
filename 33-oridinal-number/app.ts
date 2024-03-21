951fd21418d19f92127c5867cadf542d07117ddc
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-21 17:25:43 +0000

    done

---
+++ 33-oridinal-number/app.ts
@@ -0,0 +1,20 @@
+/*
+<<<< Task No: 33 >>>>
+Ordinal Numbers: 
+Ordinal numbers indicate their position in a array, 
+such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+*/
+
+
+let ordinalNums: (number | string)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+ordinalNums.forEach((num, i, arr) => {
+    switch(num) {
+        case 1: arr[i] = `${num}st`; break;
+        case 2: arr[i] = `${num}nd`; break;
+        case 3: arr[i] = `${num}rd`; break;
+        default: arr[i] = `${num}th`;
+    }
+    console.log(ordinalNums[i]);
+});
+
