9e08e027c12acc54a2e0c2b63cc538305242346f
Muhammad Nuaman <119972317+muhammad-nuaman@users.noreply.github.com>
2024-03-21 17:54:25 +0000

    done

---
+++ 34-pizza/app.ts
@@ -0,0 +1,33 @@
+/*
+<<<< Task No: 33 >>>>
+Pizzas: 
+Think of at least three kinds of your favorite pizza. 
+Store these pizza names in a array, and then use a for loop to 
+print the name of each pizza.
+*/
+
+// Pizzas
+const pizzas: string[] = [
+    "Margherita",
+    "Pepperoni",
+    "Vegetarian",
+    "Hawaiian"
+];
+// Opinions About Pizzas
+const opinions: string[] = [
+    'pizza because of its simplicity and the fresh taste of basil and tomatoes.',
+
+    'pizza is my favorite due to its savory pepperoni slices and gooey cheese combination.',
+    
+    'pizza is a great choice for me as I enjoy the variety of colorful vegetables and flavors it offers.',
+    
+    'pizza is a unique blend of sweet and savory with the combination of pineapple and ham, which I find quite delicious.'
+];
+
+
+// Print Opinions
+// for (let i = 0; i < pizzas.length; i++) console.log(`${pizzas[i]} ${opinions[i]} \n`);
+pizzas.forEach(pizza => opinions.forEach(opinion => console.log(`${pizzas} ${opinion}\n`)));
+
+// Print simple statement
+console.log('"I really like pizza!"');
