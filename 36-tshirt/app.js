56e3cbf1af4ff0aec5ce5c462dbbf7479a6776bf
muhammad-nuaman <muhammadnuaman92@gmail.com>
2024-03-22 21:03:43 +0500

    done

--- 36-tshirt/app.js
+++ 36-tshirt/app.js
@@ -1,11 +1,18 @@
 /*
 <<<<< Task No 36 >>>>>>
+<<<<< Task No 37 >>>>>>
+
 T-Shirt:
 Write a function called make_shirt()
 that accepts a size and the text of a message that should be
 printed on the shirt.
 The function should print a sentence summarizing the size of
 the shirt and the message printed on it. Call the function.
+
+Large Shirts:
+Modify the make_shirt() function so that shirts are large by default with
+a message that reads I love TypeScript. Make a large shirt and a medium
+shirt with the default message, and a shirt of any size with a different message.
 */
 // Make Shirts
 function make_shirt(size = 'Large', color = 'White', design = 'Typography', message = 'I love Typescript') {
--- 36-tshirt/app.ts
+++ 36-tshirt/app.ts
@@ -1,15 +1,14 @@
 /*
 <<<<< Task No 36 >>>>>>
+<<<<< Task No 37 >>>>>>
+
 T-Shirt:
 Write a function called make_shirt() 
 that accepts a size and the text of a message that should be 
 printed on the shirt. 
 The function should print a sentence summarizing the size of 
 the shirt and the message printed on it. Call the function.
-*/
 
-/*
-<<<<< Task No 37 >>>>>>
 Large Shirts: 
 Modify the make_shirt() function so that shirts are large by default with 
 a message that reads I love TypeScript. Make a large shirt and a medium
