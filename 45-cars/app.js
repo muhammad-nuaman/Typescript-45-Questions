242e01a6aeb94432389b00ddcf5a2ab041de5dcb
muhammad-nuaman <muhammadnuaman92@gmail.com>
2024-03-27 16:50:11 +0500

    done

---
+++ 45-cars/app.js
@@ -0,0 +1,19 @@
+/*
+<<<<< Task No: 44 >>>>>
+Sandwiches:
+Write a function that accepts a array of items a person wants on a sandwich.
+The function should have one parameter that collects as many items as the
+function call provides, and it should print a summary of the sandwich that is
+being ordered. Call the function three times, using a different number of
+arguments each time.
+*/
+// Define Sandwich
+function sandwich(...order) {
+    console.count('\nSandwich Order');
+    order.forEach(item => console.log(`● ${item}`));
+}
+// Sandwich Orders
+sandwich('white bread', 'tomato ketchup', 'cucumber and tomato', 'chicken spread');
+sandwich('brown bread', 'chilli sauce', 'tomato', 'chicken spread');
+sandwich('white bread', 'tomato ketchup', 'cucumber', 'veg spread');
+export {};
