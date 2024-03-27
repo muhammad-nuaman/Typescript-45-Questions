eb6b8245dee74890d77312be2ec8b0f7ba7b4506
muhammad-nuaman <muhammadnuaman92@gmail.com>
2024-03-27 16:51:17 +0500

    done

--- 44-sandwiches/app.ts
+++ 44-sandwiches/app.ts
@@ -1,8 +1,29 @@
 /*
-<<<<< Task No: 45 >>>>>
-Cars: Write a function that stores information about a car in a Object. 
-The function should always receive a manufacturer and a model name. 
-It should then accept an arbitrary number of keyword arguments. 
-Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+<<<<< Task No: 44 >>>>>
+Sandwiches: 
+Write a function that accepts a array of items a person wants on a sandwich.
+The function should have one parameter that collects as many items as the
+function call provides, and it should print a summary of the sandwich that is
+being ordered. Call the function three times, using a different number of
+arguments each time.
 */
 
+// Define Ingredients
+type ingredients = [
+    'white bread' | 'brown bread',
+    'tomato ketchup' | 'chilli sauce',
+    'cucumber and tomato' | 'cucumber' | 'tomato',
+    'chicken spread' | 'veg spread'
+];
+
+// Define Sandwich
+function sandwich(...order: ingredients) {
+    console.count('\nSandwich Order');
+    order.forEach(item => console.log(`● ${item}`)
+    );
+}
+
+// Sandwich Orders
+sandwich('white bread', 'tomato ketchup', 'cucumber and tomato', 'chicken spread');
+sandwich('brown bread', 'chilli sauce', 'tomato', 'chicken spread');
+sandwich('white bread', 'tomato ketchup', 'cucumber', 'veg spread');
