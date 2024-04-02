9f943e00df1b460056d9848899f8622a134a33ae
muhammad-nuaman <muhammadnuaman92@gmail.com>
2024-04-02 17:58:40 +0500

    done

--- 05-famous-quote-2/app.js
+++ 05-famous-quote-2/app.js
@@ -7,13 +7,15 @@ the famous person’s name in a variable called famous_person.
 Then compose your message and store it in a new variable called message.
 Print your message.
 */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+// Import Quote
+const quotes_js_1 = __importDefault(require("../modules/quotes.js"));
 // Declare Person Name
 let famous_person = 'Muhammad Ali Jinnah';
-// Declare Person's Quote
-let quote = 'Think 100 times before you take a decision \n' +
-    'But once that decision is taken, stand by it as one man';
 // Declare Message
-let message = `${famous_person} once said,\n "${quote}"`;
+let message = `${famous_person} once said,\n "${quotes_js_1.default}"`;
 // Print Message
 console.log(message);
--- 05-famous-quote-2/app.ts
+++ 05-famous-quote-2/app.ts
@@ -7,13 +7,12 @@ Then compose your message and store it in a new variable called message.
 Print your message.
 */
 
+// Import Quote
+import quote from '../modules/quotes.js'
+
 // Declare Person Name
 let famous_person:string = 'Muhammad Ali Jinnah';
 
-// Declare Person's Quote
-let quote:string = 
-    'Think 100 times before you take a decision \n'+ 
-    'But once that decision is taken, stand by it as one man';
 
 // Declare Message
 let message:string =`${famous_person} once said,\n "${quote}"`;
