1fb0f8fd72567e6bee675d6ac04264729a4efb71
muhammad-nuaman <muhammadnuaman92@gmail.com>
2024-04-08 18:34:57 +0500

    done

--- 09-fav-number/app.ts
+++ 09-fav-number/app.ts
@@ -1,12 +1,14 @@
 /*
 <<<< Task No: 9 >>>> 
-Favorite Number: 
-Store your favorite number in a variable. 
-Then, using that variable, create a message that reveals 
-your favorite number. Print that message.
+
+# Favorite Number: 
+
+> Store your favorite number in a variable. Then, using that variable, 
+create a message that reveals your favorite number. Print that message.
+
 */
 
-// Declare Favorite Number
+// Define Favorite Number
 let fav_num: number = 7;
 
 // Print Favorite Number
